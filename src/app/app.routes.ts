4e73acb58cdb3f3a04897dd2d5458ad8531db85a
Danil Biryukov <danilbiryukov04@gmail.com>
2024-03-03 16:40:43 +0300

    Сделал заготовки стр для home, login, signup, profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home',
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login',
+    },
+    {
+        path: 'signup',
+        component: SignupComponent,
+        title: 'Signup',
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        title: 'Profile',
+    },
+    {
+        path: '**',
+        component: HomeComponent,
+        redirectTo:'',
+    },
+];
