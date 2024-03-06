ab869a9778a6385a4534270dc9d4104b5c4c97bd
Danil Biryukov <danilbiryukov04@gmail.com>
2024-03-06 21:47:31 +0300

    Сделал login и signup. В глобальные стили добавил input и btn

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-login',
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, RouterLink]
 })
 export class LoginComponent {
 
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-signup',
     standalone: true,
     templateUrl: './signup.component.html',
     styleUrl: './signup.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, RouterLink]
 })
 export class SignupComponent {
 
