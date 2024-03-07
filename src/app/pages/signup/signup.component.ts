d9fdd5c6a4a1b1156024b1af84299b51484baa70
Danil Biryukov <danilbiryukov04@gmail.com>
2024-03-08 00:37:05 +0300

    В signup сделал проверки, которые потом надо будет дополнить.

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { RouterLink } from "@angular/router";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators  } from '@angular/forms';
 
 @Component({
     selector: 'app-signup',
     standalone: true,
     templateUrl: './signup.component.html',
     styleUrl: './signup.component.scss',
-    imports: [HeaderComponent, RouterLink]
+    imports: [HeaderComponent, RouterLink, ReactiveFormsModule ]
 })
-export class SignupComponent {
 
+export class SignupComponent {
+    userData: FormGroup;
+    formSubmitted: boolean = false;
+notOnlyWhitespaceValidator: any;
+
+    constructor() {
+        this.userData = new FormGroup({
+            email: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+        });
+    }
+
+    onSubmit() {
+        this.formSubmitted = true;
+        if (this.userData.valid) {
+            console.log(this.userData.value);
+        } else {
+            console.log("Form is not valid.");
+        }
+    }
 }
